6770e7e1f26065ec304110f140e4181eab4df06e
umiigo <dev.umigo@gmail.com>
2019-06-25 20:19:19 +0100

    cleaned up devise login and signup routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,24 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
+         :recoverable, :rememberable, :validatable, :omniauthable
 
-  validates :full_name, presence: true, length: {maximum: 50}, :omniauthable
-
+  validates :full_name, presence: true, length: {maximum: 50}
 
   def self.from_omniauth(auth)
-    user = User.where(email : auth.info.email.first)
+    user = User.where(email: auth.info.email).first
+
     if user
       return user
     else
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
         user.email = auth.info.email
         user.password = Devise.friendly_token[0, 20]
-        user.name = auth.info.name   # assuming the user model has a name
-        user.image = auth.info.image # assuming the user model has an image
-        # If you are using confirmable and the provider(s) you use validate emails, 
-        # uncomment the line below to skip the confirmation emails.
-        # user.skip_confirmation!
-
+        user.full_name = auth.info.name
+        user.image = auth.info.image
         user.uid = auth.uid
         user.provider = auth.provider
+
       end
     end
   end
