d7539c227a465ce6f523627db1e0e25c0bbaf051
umiigo <dev.umigo@gmail.com>
2019-07-22 20:19:19 +0100

    added upload image function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,22 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  
-  
+
   def dashboard
   end
 
   def update
     @user = current_user
-    if @user.update_attribute(current_user_params)
-        flash[:notice] = "Saved..."
+    if @user.update_attributes(current_user_params)
+      flash[:notice] = "Saved..."
     else
-        flash[:alert] = "Something Went Wrong..."
+      flash[:alert] = "Cannot update..."
     end
     redirect_to dashboard_path
   end
 
   private
   def current_user_params
-    params.require(:user).permit(:from, :about, :status, :language)
+    params.require(:user).permit(:from, :about, :status, :language, :avatar)
   end
 end
 
