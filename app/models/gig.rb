5ca285cea8be246950cf126e1f89694eeb97779f
umiigo <dev.umigo@gmail.com>
2019-09-04 20:19:19 +0100

    added order  model

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -3,8 +3,14 @@ class Gig < ApplicationRecord
   belongs_to :category
 
   has_many :pricings
+  has_many :orders
+
+  has_many :buying_orders, foreign_key: "buyer_id", class_name: "Order"
+  has_many :selling_orders, foreign_key: "seller_id", class_name: "Order"
+
   has_many_attached :photos
   has_rich_text :description
+  
 
   accepts_nested_attributes_for :pricings
 
