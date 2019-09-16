ea76da4eeaf6cac6cb8ca1e9c1662c0584d27c80
umiigo <dev.umigo@gmail.com>
2019-09-16 20:19:19 +0100

    updated routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+
   root 'pages#home'
 
   get '/dashboard', to: 'users#dashboard'
   get '/users/:id', to: 'users#show'
   get '/selling_orders', to: 'orders#selling_orders'
   get '/buying_orders', to: 'orders#buying_orders'
+  
   post '/users/edit', to: 'users#update'
 
   put '/orders/:id/complete', to: 'orders#complete', as: 'complete_order'
