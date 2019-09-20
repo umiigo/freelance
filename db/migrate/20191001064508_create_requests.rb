5ca7d0c38815aa459e54b4bad5b13b3ef42d443b
umiigo <dev.umigo@gmail.com>
2019-09-20 20:19:19 +0100

    added request model

---
+++ db/migrate/20191001064508_create_requests.rb
@@ -0,0 +1,13 @@
+class CreateRequests < ActiveRecord::Migration[6.0]
+  def change
+    create_table :requests do |t|
+      t.text :description
+      t.string :title
+      t.integer :budget
+      t.references :user, null: false, foreign_key: true
+      t.references :categories, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
