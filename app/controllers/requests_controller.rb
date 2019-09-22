b3fd8cb1e49582078c476e6c6642e0338f121dfc
umiigo <dev.umigo@gmail.com>
2019-09-22 20:19:19 +0100

    added controller structure

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,4 +1,9 @@
 class RequestsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_request, except: [:new, :create, :index, :list]
+  before_action :is_authorised, only: [:edit, :update, :destroy]
+  
+
   def index
   end
 
@@ -21,6 +26,14 @@ class RequestsController < ApplicationController
   end
 
   private
+
+  def set_requests
+    @request = Request.find(params[:id])
+  end
+
+  def is_authorised
+    redirect_to root_path, alert: "You do not have permission" unless current_user.id == @request.user_id
+  end
 
   def request_params
     params.require(:request).permit(:description, :category_id, :delivery, :budget, :attachment_file, :title)
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,12 @@
 Rails.application.routes.draw do
-
-  get 'requests/index'
-  get 'requests/new'
-  get 'requests/create'
-  get 'requests/edit'
-  get 'requests/update'
-  get 'requests/destroy'
-  get 'requests/list'
   root 'pages#home'
 
   get '/dashboard', to: 'users#dashboard'
   get '/users/:id', to: 'users#show'
   get '/selling_orders', to: 'orders#selling_orders'
   get '/buying_orders', to: 'orders#buying_orders'
-  
+  get '/all-requests', to: 'requests#list'
+
   post '/users/edit', to: 'users#update'
 
   put '/orders/:id/complete', to: 'orders#complete', as: 'complete_order'
@@ -26,6 +19,8 @@ Rails.application.routes.draw do
     end
     resources :orders, only: [:create] 
   end
+
+  resources :requests
 
   devise_for :users, 
               path: '', 
