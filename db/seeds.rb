0fc67b354d21448c00348a28cd3683ad9458284e
umiigo <dev.umigo@gmail.com>
2019-09-30 20:19:19 +0100

    added list category page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,20 @@
-5.times do
-    user = User.create(
-        full_name: Faker::Name.name,
-        email: Faker::Internet.free_email,
-        about: Faker::Quote.matz,
-        password: '123456',
-        from: Faker::Address.country,
-        language: Faker::Nation.language,
-        created_at: Date.today
-    )
+# 5.times do
+#     user = User.create(
+#         full_name: Faker::Name.name,
+#         email: Faker::Internet.free_email,
+#         about: Faker::Quote.matz,
+#         password: '123456',
+#         from: Faker::Address.country,
+#         language: Faker::Nation.language,
+#         created_at: Date.today
+#     )
 
-    user.avatar.attach(                
-        io: image = open("https://i.pravatar.cc/300"),
-        filename: "avatar#{user.id}.jpg", 
-        content_type: 'image/jpg'
-    )
-end 
+#     user.avatar.attach(                
+#         io: image = open("https://i.pravatar.cc/300"),
+#         filename: "avatar#{user.id}.jpg", 
+#         content_type: 'image/jpg'
+#     )
+# end 
 
 # Create dummy Requests
 
