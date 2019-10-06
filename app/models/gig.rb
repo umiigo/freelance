363fd1cbb25b6c506d08a4aada8a19a65a8001aa
umiigo <dev.umigo@gmail.com>
2019-10-06 20:19:19 +0100

    created review toggle and view

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -4,6 +4,7 @@ class Gig < ApplicationRecord
 
   has_many :pricings
   has_many :orders
+  has_many :reviews
 
   has_many_attached :photos
   has_rich_text :description
@@ -12,5 +13,10 @@ class Gig < ApplicationRecord
   accepts_nested_attributes_for :pricings
 
   validates :title, presence: {message: 'cannot be blank'}
+
+  def average_rating
+   reviews.count==0? 0 : reviews.average(:stars).round(1)
+    
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,13 @@ Rails.application.routes.draw do
 
   post '/users/edit', to: 'users#update'
   post '/offers', to: 'offers#create'
+  post '/reviews', to: 'reviews#create'
 
 
   put '/orders/:id/complete', to: 'orders#complete', as: 'complete_order'
   put '/offers/:id/accept', to: 'offers#accept', as: 'accept_offer'
   put '/offers/:id/reject', to: 'offers#reject', as: 'reject_offer'
+
 
   resources :gigs do
     member do
