e2cd833a0a6afb4a85d70a03419a999a82b46070
umiigo <dev.umigo@gmail.com>
2019-10-07 20:19:19 +0100

    updated  review controller to handle view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,32 @@
 class ReviewsController < ApplicationController
     def create
+        order = Order.find(review_params[:order_id])
+
+        if order && current_user.id == order.buyer.id
+            if Review.exists?(order_id: review_params[:order_id], buyer_id: current_user.id)
+                flash[:alert] = 'You have already reviewed this order'
+            else 
+                review = Review.new(review_params)
+                review.gig = order.gig
+                review.buyer = current_user
+                review.seller = order.seller
+
+                if review.save
+                    flash[:notice] = "Review sent successfully"
+                else
+                    flash[:notice] = "Something went wrong!"
+                end
+            end
+        else
+            flash[:alert] = 'Invalid Order'
+        end
+        redirect_to request.referrer
     end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:stars, :review, :order_id)
+    end
+
 end
